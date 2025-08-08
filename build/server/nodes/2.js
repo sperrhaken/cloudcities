800e62a119e26934f607ac0a51acba6b00cbbf6f
Felix Dietrich <felix.dietrich@sperrhaken.name>
2025-08-08 20:56:45 +0200

    Remove build, node_modules, and .svelte-kit

--- build/server/nodes/2.js
+++
@@ -1,6 +0,0 @@
-import * as module from '../entries/pages/draw.svelte.js';
-
-export { module };
-export const entry = 'pages/draw.svelte-b1a3fdae.js';
-export const js = ["pages/draw.svelte-b1a3fdae.js","chunks/index-ffce5867.js","chunks/wpTitles-5387dbe1.js","chunks/index-c54c4907.js"];
-export const css = ["assets/pages/draw.svelte-8a68ea2d.css","assets/wpTitles-09c7061f.css"];
